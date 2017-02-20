09e63f9d4d783391a4fa1d23ea503a1f5cccbadb
filmninja <dan@greenlightevents.us>
2017-02-19 18:36:43 -0800

    Added styles and mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,17 @@ var gulp 			= require('gulp'),
 	autoprefixer 	= require('autoprefixer'),
 	cssvars		 	= require('postcss-simple-vars'),
 	nested		 	= require('postcss-nested'),
+	mixins		 	= require('postcss-mixins'),
 	cssimport		= require('postcss-import');
 	
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([ cssimport, cssvars, autoprefixer, nested ]))
+		.pipe(postcss([ cssimport, mixins, cssvars, autoprefixer, nested ]))
+		.on('error', function(err){
+			console.log(err.toString());
+			this.emit('end');
+		})
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
